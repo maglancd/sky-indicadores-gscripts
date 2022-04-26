a9e63bdb2a73c28826e831dabe56724bb47cba67
maglancd <maglancd@gmail.com>
2022-04-26 20:05:36 -0300

    commit 2022.04.26 20:05

--- webapp.js
+++ webapp.js
@@ -1,23 +1,28 @@
 function processarJSON(contentJSON) {
 
     /*
-      { "escrituras": {
-          "sobreescrever": true,
-          "linha": 2,
-          "coluna": 1,
-          "dados": [{
-              "mes": "janeiro",
-              "ano": "2020",
-              "quantidade": "5",
-              "categoria": "inventario"
-          }, {
-              "mes": "fevereiro",
-              "ano": "2020",
-              "quantidade": "15",
-              "categoria": "compra e venda"
-          }]
-       } 
-     }
+    {   "usarScriptCache": false,    
+        "idArquivoPlanilha": "",
+        "planilhas": [{ 
+            "idArquivoPlanilha": "1CUQ5ipCY1YipSBRlqiE_3bNnHwKRg7w77H-tpZpN76w",  
+            "nomePlanilha": "escrituras",      
+            "sobrescrever": false,
+            "linha": 1,
+            "coluna": 1,
+            "colunaFinal": 3,
+            "dados": [{
+                "mes": "janeiro",
+                "ano": "2020",
+                "quantidade": "5",
+                "categoria": "inventario"
+            }, {
+                "mes": "fevereiro",
+                "ano": "2020",
+                "quantidade": "15",
+                "categoria": "compra e venda"
+            }]
+        }] 
+        }
      */
     clearDebug();
     contentJSON = transmitterParametros.parametros
@@ -26,7 +31,7 @@ function processarJSON(contentJSON) {
             SS = SpreadsheetApp.openById(contentJSON['idArquivoPlanilha'])
     }
 
-    if (contentJSON.hasOwnProperty('planihas'))
+    if (contentJSON.hasOwnProperty('planilhas'))
         for (var i in contentJSON.planilhas) {
             var dados = contentJSON.planilhas[i];
 
@@ -61,11 +66,11 @@ function processarDadosAbaPlanilha(dados) {
 
     if (dados.dados.length > 0) {
 
-        var aba = SS.getSheetByName(dados.nomeAba);
+        var aba = SS.getSheetByName(dados.nomePlanilha);
         if (aba == null) {
-            debug('criando aba: ' + dados.nomeAba)
+            debug('criando aba: ' + dados.nomePlanilha)
             aba = SS.insertSheet()
-            aba.setName(dados.nomeAba);
+            aba.setName(dados.nomePlanilha);
 
             // adicionar cabecalho
             cabecalho = Object.keys(dados.dados[0]);
@@ -79,16 +84,17 @@ function processarDadosAbaPlanilha(dados) {
             aba.getRange(linha, coluna).offset(0, 0, cabecalhoTranspose.length, cabecalhoTranspose[0].length).setValues(cabecalhoTranspose);
         }
 
-        var colunaFinal = dados.getLastColumn()
+        var colunaFinal = aba.getLastColumn()
         if (dados.hasOwnProperty('colunaFinal'))
             colunaFinal = dados.colunaFinal;
+        var linhaFinal = aba.getLastRow();
 
 
         if (cabecalho == null) {
             try {
                 cabecalho = aba.getRange(linha, coluna, 1, colunaFinal).getValues()[0];
                 if (sobrescrever)
-                    aba.getRange(linha + 1, coluna, dados.getLastRow(), colunaFinal).clearContent();
+                    aba.getRange(linha + 1, coluna, linhaFinal, colunaFinal).clearContent();
             } catch { }
         }
 
@@ -100,7 +106,7 @@ function processarDadosAbaPlanilha(dados) {
 
                 var linhaDados = linha + 1;
                 if (!sobrescrever)
-                    linhaDados = dados.getLastRow() + 1;
+                    linhaDados = linhaFinal + 1;
 
                 aba.getRange(linhaDados, coluna).offset(0, 0, registros.length, registros[0].length).setValues(registros);
             }
